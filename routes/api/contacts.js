596079688aec80111588b10e70b20a9885b4ea17
h0wter <howter99@gmail.com>
2023-02-07 00:17:27 +0200

    add isValidId middleware and patch route

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,22 +4,35 @@ import { deleteById } from "../../controllers/contacts/deleteById.js";
 import { getAll } from "../../controllers/contacts/getAll.js";
 import { getById } from "../../controllers/contacts/getById.js";
 import { updateById } from "../../controllers/contacts/updateById.js";
+import { updateStatusContact } from "../../controllers/contacts/updateStatusContact.js";
 import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
+import { isValidId } from "../../middlewares/isValidId.js";
 import { validationBody } from "../../middlewares/validationBody.js";
-import { addContactSchema } from "../../schemas/contacts.js";
+import {
+  addContactSchema,
+  updateStatusContactSchema,
+} from "../../models/contacts.js";
 
 export const contactsRouter = express.Router();
 
 contactsRouter.get("/", ctrlWrapper(getAll));
 
-contactsRouter.get("/:contactId", ctrlWrapper(getById));
+contactsRouter.get("/:contactId", isValidId, ctrlWrapper(getById));
 
 contactsRouter.post("/", validationBody(addContactSchema), ctrlWrapper(add));
 
-contactsRouter.delete("/:contactId", ctrlWrapper(deleteById));
+contactsRouter.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validationBody(updateStatusContactSchema),
+  ctrlWrapper(updateStatusContact)
+);
+
+contactsRouter.delete("/:contactId", isValidId, ctrlWrapper(deleteById));
 
 contactsRouter.put(
   "/:contactId",
+  isValidId,
   validationBody(addContactSchema),
   ctrlWrapper(updateById)
 );
