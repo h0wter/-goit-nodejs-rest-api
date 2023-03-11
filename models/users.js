e273bfae24076c60146d93ccb81d6cf6219ca3be
h0wter <howter99@gmail.com>
2023-03-11 20:24:27 +0200

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,15 +1,18 @@
 import { Schema, model } from "mongoose";
-// import Joi from "joi";
+import Joi from "joi";
+import { handleSchemaValidationErrors } from "../helpers/handleSchemaValidationErrors.js";
+
+const emailRegexp = /^[\w.]+@[\w]+.[\w]+$/;
 
 const userSchema = new Schema({
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
   email: {
     type: String,
     required: [true, "Email is required"],
     unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
   },
   subscription: {
     type: String,
@@ -20,6 +23,23 @@ const userSchema = new Schema({
     type: String,
     default: null,
   },
+});
+
+userSchema.post("save", handleSchemaValidationErrors);
+
+export const registerSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).max(20).required(),
+  subscription: Joi.string().valid("starter", "pro", "business"),
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).max(20).required(),
+});
+
+export const updateSubscriptionSchema = Joi.object({
+  subscription: Joi.string().valid("starter", "pro", "business").required(),
 });
 
 export const User = model("user", userSchema);
