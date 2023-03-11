f29d3ca63887ffc8c1227e26e7480473470884a1
h0wter <howter99@gmail.com>
2023-03-11 20:22:56 +0200

    Update getAll.js
    added pagination, filtration by fields favorite and id

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,6 +1,16 @@
 import { Contact } from "../../models/contacts.js";
 
-export const getAll = async (_, res) => {
-  const result = await Contact.find();
+export const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 5, favorite = false } = req.query;
+
+  const favoriteFilter = favorite === "true" ? [true] : [true, false];
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find(
+    { owner, favorite: { $in: favoriteFilter } },
+    { __v: 0 },
+    { skip, limit: Number(limit) }
+  ).populate("owner", "email");
   res.json(result);
 };
