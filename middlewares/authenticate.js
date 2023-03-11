368fad0421c7d8991c0b2b0d3d5add745f5a8c63
h0wter <howter99@gmail.com>
2023-03-11 20:23:42 +0200

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,14 +1,20 @@
 import jwt from "jsonwebtoken";
 import { RequestError } from "../helpers/RequestError.js";
+import { User } from "../models/users.js";
 
-export const authenticate = async (req, res, next) => {
+const { SECRET_KEY } = process.env;
+
+export const authenticate = async (req, _, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") next(RequestError(401, "Not authorized"));
   try {
-    const { id } = jwt.verify(token, SECRET_KEY);
+    jwt.verify(token, SECRET_KEY);
+    const user = await User.findOne({ token });
+    if (!user) next(RequestError(401, "Not authorized"));
+    req.user = user;
+    next();
   } catch (error) {
     next(RequestError(401, error.message));
   }
-  next();
 };
