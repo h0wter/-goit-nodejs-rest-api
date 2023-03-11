0d4457f583b818e281ed81afb8a9d19ef638a2d3
h0wter <howter99@gmail.com>
2023-03-11 20:08:23 +0200

    removed dotenv

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import * as dotenv from "dotenv";
 import { contactsRouter } from "./routes/api/contacts.js";
-import { authRouter } from "./routes/api/auth.js";
+import { usersRouter } from "./routes/api/users.js";
 import { authenticate } from "./middlewares/authenticate.js";
-dotenv.config();
 
 export const app = express();
 
@@ -15,7 +13,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
 app.use("/api/contacts", authenticate, contactsRouter);
 
 app.use((_, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+import "dotenv/config";
 import mongoose from "mongoose";
 import { app } from "./app.js";
 
 const { PORT = 3000, DB_HOST } = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
