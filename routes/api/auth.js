6878b441efbb3eb29bfd6fc0974952116853f2d7
h0wter <howter99@gmail.com>
2023-03-11 20:08:41 +0200

    Delete auth.js

--- routes/api/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { register } from "../../controllers/auth/register.js";
-import { login } from "../../controllers/auth/login.js";
-import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
-
-export const authRouter = express.Router();
-
-authRouter.post("/register", ctrlWrapper(register));
-authRouter.post("/login", ctrlWrapper(login));
