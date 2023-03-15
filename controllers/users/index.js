b6acb406f734b6b59f2499da172e1bfac6bcbfee
h0wter <howter99@gmail.com>
2023-03-15 21:05:43 +0200

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -3,5 +3,6 @@ import { login } from "./login.js";
 import { logout } from "./logout.js";
 import { register } from "./register.js";
 import { updateSubscription } from "./updateSubscription.js";
+import { updateAvatar } from "./updateAvatar.js";
 
-export { current, login, logout, register, updateSubscription };
+export { current, login, logout, register, updateSubscription, updateAvatar };
